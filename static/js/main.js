74560b7b76537f85fb340ccbc266605789778a92
Your Name <you@example.com>
2017-06-06 10:05:21 +0500

    доступ

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
   //~ var atopnav = $("a.top-nav");
   //~ if (atopnav.get(0)) atopnav.
   //~ var atopnav = $("a.right-side");
-  $("a.right-side").sideNav({menuWidth: $('body').innerWidth()*2/4, edge: 'right', closeOnClick: false,  draggable: true, onClick: function(side, opts) {side.menu.css('width', '51%')}});// Initialize menu button
+  $("nav.top a.right-side").sideNav({menuWidth: $('body').innerWidth()*2/4, edge: 'right', closeOnClick: false,  draggable: true, onClick: function(side, opts) {side.menu.css('width', '51%')}});// Initialize menu button
+  $("nav.top a.left-side").sideNav({menuWidth: $('body').innerWidth()*2/4, edge: 'left', closeOnClick: false,  draggable: true, onClick: function(side, opts) {side.menu.css('width', '51%')}});// Initialize menu button
   
   var search = $('#search');
   if (!search.get(0)) return;
--- templates/header.html.cgi.pl
+++ templates/header.html.cgi.pl
@@ -1,16 +1,27 @@
 my $signed = $c->auth_user->{id}
   if $c->is_user_authenticated;
 
+my $login_li = sub {
+  return li({},
+          a({-class=>"btn-large000 black-text000 teal000 lighten-1000", -href=>$c->url_for('profile')->query(from=>$c->url_for->path), -title=>"Вход/Регистрация",},#btn-large000 black-text000 teal000 lighten-1000
+            i({-class=>"material-icons",}, 'exit_to_app'),
+            span({-class=>"hide-on-small-only",}, 'Вход',),
+            
+          ),
+        )
+        if $c->match->endpoint && $c->match->endpoint->name ne 'profile';
+  return ();
+  
+};
+
 header(
 div({-class=>"header clearfix",},
 
 nav({-class=>"top"},
 div({-class=>"nav-wrapper",},
   div({-class=>"logo left",},
-    a({-href=>"/", -class=>"brand-logo1 btn-floating btn-large white",},
-      #~ img({-src=>"/i/truck-48-237857.png", -alt=>"", -style=>"width:70%; vertical-align: middle;"}),
+    a({-href000=>"/", -class=>"brand-logo1 btn-floating btn-large white left-side", 'data-activates'=>"left-side-top-nav", -href=>"javascript:",},
       img({-src=>"/apple-touch-icon.png", -alt=>"", -style=>"width:70%; vertical-align: middle;"}),
-      #~ i({-class=>"material-icons  black-text",}, 'home'),
     ),
   ),#$c->config('Проект') <i class="material-icons">album</i>
   
@@ -18,45 +29,39 @@ div({-class=>"nav-wrapper",},
   
   #~ div({-class=>"form-search right", -style=>"width: 55%; margin-right: 0;"}, $c->include('forms/search',),),
   
-  ul({-class=>"fixed-action-btn horizontal click-to-toggle", -style=>"position:absolute; top: 0px; right: 0px;",},#hide-on-med-and-down0100
+  (!$signed && $login_li->()) && ul({-class=>"fixed-action-btn horizontal click-to-toggle", -style=>"position:absolute; top: 0px; right: 0px;",},#hide-on-med-and-down0100
     
     $signed
-    ? li({-class=>"notifications-container", }, a({-class=>"button-collapse000 btn-floating btn-large black right-side full hide-on-large-only000", -style=>"", 'data-activates'=>"right-side-top-nav", -href=>"javascript:", },#'cachedAjaxScript'=>"/js/materialize/sideNav.js",
-      (0 || '') && span({-class=>"notifications-container", },
-       span({-class=>"notifications-icon overlay grow infinite yellow darken-2 white-text ", -style=>"right: 10px; top: 10px;"}, ''),#span({-class=>"notifications-icon-count"},'!')
-       span({-class=>"notification-inner",},    )
+    ? li({-class=>"notifications-container", }, 
+      a({-class=>"button-collapse000 btn-floating btn-large black right-side full hide-on-large-only000", -style=>"", 'data-activates'=>"right-side-top-nav", -href=>"javascript:", },#'cachedAjaxScript'=>"/js/materialize/sideNav.js",
+        (0 || '') && span({-class=>"notifications-container", },
+         span({-class=>"notifications-icon overlay grow infinite yellow darken-2 white-text ", -style=>"right: 10px; top: 10px;"}, ''),#span({-class=>"notifications-icon-count"},'!')
+         span({-class=>"notification-inner",},    )
+        ),
+        i({-class=>"material-icons teal lighten-1 black-text",},'menu'),#
       ),
-      i({-class=>"material-icons teal lighten-1 black-text",},'menu'),#
-    ),
     
-  )
-    : $c->match->endpoint && $c->match->endpoint->name ne 'profile'
-      ? li({},
-          a({-class=>"btn-large000 black-text000 teal000 lighten-1000", -href=>$c->url_for('profile')->query(from=>$c->url_for->path), -title=>"Вход/Регистрация",},#btn-large000 black-text000 teal000 lighten-1000
-            i({-class=>"material-icons",}, 'exit_to_app'),
-            span({-class=>"hide-on-small-only",}, 'Вход',),
-            
-          ),
-        )# icon 
-        
-      : (),
+    )
+    : $login_li->(),
   ),
   
   #~ $c->stash("контент в верхней навигации") && div({-class=>"right"}, $c->stash("контент в верхней навигации")),
 
-($signed || '') && div({-id=>"right-side-top-nav", -class=>"side-nav", },
+div({-id=>"left-side-top-nav", -class=>"side-nav", },
   
   ul({-style=>"margin:0;",},#jq-dropdown-menu
   
-  li({}, a({-class00=>"", -href=>$c->url_for('движение ДС'),}, i({-class=>"material-icons",}, 'account_balance'), 'Движение денег', ), ),
+  li({}, a({-class00=>"", -href=>$c->url_for('home'),}, i({-class=>"material-icons",}, 'home'), 'Начало системы', ), ),
   
-  li({}, a({-class00=>"", -href=>$c->url_for('отчет ДС'),}, i({-class=>"material-icons",}, 'report'), 'Отчет по деньгам', ), ),
+  ($signed || '') && li({}, a({-class00=>"", -href=>$c->url_for('движение ДС'),}, i({-class=>"material-icons",}, 'account_balance'), 'Движение денег', ), ),
   
-  li({}, a({-class00=>"", -href=>$c->url_for('profile')->query(from=>$c->url_for->path),}, i({-class=>"material-icons",}, 'person'), 'Профиль', ), ),
+  ($signed || '') && li({}, a({-class00=>"", -href=>$c->url_for('отчет ДС'),}, i({-class=>"material-icons",}, 'report'), 'Отчет по деньгам', ), ),
   
-  li({}, a({-class00=>"", -href=>$c->url_for('logout')->query(from=>$c->url_for->path),}, i({-class=>"material-icons",}, 'lock_outline'), 'Выход', )),
+  ($signed || '') && li({}, a({-class00=>"", -href=>$c->url_for('profile')->query(from=>$c->url_for->path),}, i({-class=>"material-icons",}, 'person'), 'Профиль', ), ),
   
+  ($signed || '') && li({}, a({-class00=>"", -href=>$c->url_for('logout')->query(from=>$c->url_for->path),}, i({-class=>"material-icons",}, 'lock_outline'), 'Выход', )),
   
+  (!$signed || '') && $login_li->(),
   
   li(
     a({-class=>"dropdown-button", -href=>"#!", 'data-activates'=>"dropdown123"}, 'Еще ...',
