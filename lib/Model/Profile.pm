82f82b8a53d4256f431ec824a5df8e891cb443f8
Your Name <you@example.com>
2017-06-26 17:32:31 +0500

    дальше

---
+++ lib/Model/Profile.pm
@@ -0,0 +1,37 @@
+package Model::Profile;
+use Mojo::Base 'Model::Base';
+#~ use Mojo::Util qw(dumper);
+
+#~ has sth_cached => 1;
+my $main_table = 'профили';
+has [qw(app)];
+
+
+sub new {
+  state $self = shift->SUPER::new(@_);
+  $self->{template_vars}{tables}{main} = $main_table;
+  #~ die dumper($self->{template_vars});
+  #~ $self->dbh->do($self->sth('таблицы'));
+  #~ $self->dbh->do($self->sth('функции'));
+  return $self;
+}
+
+sub список {
+  my ($self) = @_;
+  
+  $self->dbh->selectall_arrayref($self->sth('список или позиция'), {Slice=>{},}, (undef) x 2);
+  
+}
+
+
+1;
+
+__DATA__
+@@ список или позиция
+select *
+from "профили"
+where (?::int is null and not coalesce(disable, false))
+  or (id=?)
+order by names
+;
+
---
+++ static/js/components/profile/item.js
@@ -0,0 +1,108 @@
+(function () {'use strict';
+/*
+*/
+
+var moduleName = "ProfileItem";
+
+var module = angular.module(moduleName, ['AppTplCache', 'appRoutes']);//'ngSanitize',, 'dndLists'
+
+
+var Component = function  ($scope, $timeout, $http, $element, appRoutes) {
+  var $ctrl = this;
+  //~ $scope.$timeout = $timeout;
+  
+  $ctrl.$onInit = function(){
+    if(!$ctrl.data) $ctrl.data = {};
+
+    $ctrl.LoadData().then(function(){
+      $ctrl.showListBtn = (!$ctrl.data.title || $ctrl.data.title.length === 0);
+      $ctrl.ready = true;
+      
+    });
+    
+  };
+  
+  $ctrl.LoadData = function(){
+    if(!$ctrl.data.id) $ctrl.data.title = '';
+    return $http.get(appRoutes.url_for('список профилей'))
+      .then(function(resp){
+          $ctrl.autocomplete = [];
+          angular.forEach(resp.data, function(val) {
+            var title = val.names.join(' ');
+            if($ctrl.data.id  && $ctrl.data.id == val.id) $ctrl.data.title = title;
+            $ctrl.autocomplete.push({value: title, data:val});
+          });
+          
+      });
+    
+  };
+  
+  $ctrl.InitInput = function(){// ng-init input textfield
+    $ctrl.textField = $('input[type="text"]', $($element[0]));
+   
+    $ctrl.textField.autocomplete({
+      lookup: $ctrl.autocomplete,
+      //~ preserveInput: false,
+      appendTo: $ctrl.textField.parent(),
+      //~ containerClass: 'autocomplete-content dropdown-content',
+      formatResult: function (suggestion, currentValue) {
+        return arguments[3].options.formatResultsSingle(suggestion, currentValue);
+      },
+      //~ triggerSelectOnValidInput: false,
+      onSelect: function (suggestion) {
+         //~ console.log(suggestion.data);
+        //~ 
+        $timeout(function(){
+          //~ $ctrl.data=suggestion.data;
+          $ctrl.data.title=suggestion.data.names.join(' ');
+          $ctrl.data.id=suggestion.data.id;
+          $ctrl.showListBtn = false;
+        });
+        
+      },
+      onSearchComplete: function(query, suggestions){$ctrl.data._suggestCnt = suggestions.length; if(suggestions.length) $ctrl.data.id = undefined;},
+      //~ onHide: function (container) {}
+      
+    });
+    
+  };
+  
+  $ctrl.ChangeInput = function(){
+    if ( $ctrl.data.id) $ctrl.data.id = undefined;
+    $ctrl.showListBtn = ($ctrl.data.title.length === 0);
+    return true;
+  };
+  
+  
+  $ctrl.ToggleListBtn = function(){
+    var ac = $ctrl.textField.autocomplete();
+    ac.toggleAll();
+  };
+  
+  $ctrl.ClearInputBtn = function(){
+    $ctrl.data.title = '';
+    $ctrl.ChangeInput();
+    $ctrl.data._suggestCnt = 0;
+    
+  };
+  
+  
+};
+
+/*=============================================================*/
+
+module
+
+.component('profileItem', {
+  templateUrl: "profile/item",
+  //~ scope: {},
+  bindings: {
+    data: '<',
+
+  },
+  controller: Component
+})
+
+;
+
+}());
