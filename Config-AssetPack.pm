774f0b07f06d723c68ea2c59e854c0fc65fdb978
Your Name <you@example.com>
2017-08-07 10:48:40 +0500

    дальше

--- Config-AssetPack.pm
+++ Config-AssetPack.pm
@@ -3,6 +3,7 @@ use Mojo::Base -strict;
   'AssetPack::Che' => {
     pipes => [qw(Sass Css JavaScript HTML CombineFile)],
     CombineFile => {
+      version=>"2017-08-07 10:33",
       #~ url_lines =>{# только для morbo
         #~ 'js/controllers/profile/form-auth.html'=> "@@@ profile/form-auth",
         #~ 'js/c/waltex/money/work.html'=> "@@@ money/work",
