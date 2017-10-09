2c7a6e6b97152e8e13d732a0220b0c2412a27b42
Your Name <ernesto.chege@yandex.ru>
2017-10-09 12:04:14 +0500

    дальше

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,24 @@
-    /*
-  обновление скриптов работает за счет пересоздания ассет|пак кэша
-  обновление шаблонов через смену ВЕРСИИ (используется в сервисе LoadTemplateCache для добавления к урлам)
-  */
-  /*$.ajax({
-    url: "/config",
-    dataType: 'json',
-    cache: true,
-    success: function(data){
-      var config = JSON.parse(localStorage.getItem('app config') || 'false');
-      if(!config) return localStorage.setItem('app config', JSON.stringify(data));
-      if (data.VERSION && data.VERSION != config.VERSION) {
-        console.log("Перезапуск страницы с новой версией", data.VERSION);
-        localStorage.setItem('app config', JSON.stringify(data));
-        location.reload(true); 
-        config = data;
-      }
-      console.log("Конфиг", config);
-    }
-  });*/
-
 $(document).ready(function () {
+  
+  /**/
+  $('head meta[name="app-version"]').attr('content')
+  /*
+обновление скриптов работает за счет очистки/пересоздания ассет|пак кэша
+обновление шаблонов через смену ВЕРСИИ (используется в сервисе LoadTemplateCache для добавления к урлам) static/js/controllers/template-cache/script.js
+*/
+  
+  //~ var curr = {"VERSION": '2017-10-09T10:03'};
+  var curr = {"VERSION": $('head meta[name="app:version"]').attr('content') || 1};
+  var old = JSON.parse(localStorage.getItem('app uniost') || localStorage.getItem('app config') || 'false');
+  //~ if(!old) return localStorage.setItem('app config', JSON.stringify(curr));
+  if (!old || curr.VERSION != old.VERSION) {
+    console.log("Перезапуск страницы с новой версией", curr.VERSION);
+    localStorage.setItem('app config', JSON.stringify(curr));
+    location.reload(true); 
+    //~ config = data;
+  }
+  console.log("Конфиг", curr);
+  
   /*
   Навигация сайта выплывает слева или справа
   */
