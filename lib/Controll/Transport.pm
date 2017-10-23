0f54212000ab745bb15da7732fbe9fb21fb477b9
Your Name <ernesto.chege@yandex.ru>
2017-10-23 12:55:43 +0500

    дальше

--- lib/Controll/Transport.pm
+++ lib/Controll/Transport.pm
@@ -154,6 +154,7 @@ sub save_ask {
   $data->{uid} = $c->auth_user->{id}
     unless $data->{id};
   #~ $c->app->log->error($c->dumper($data));
+  delete $data->{ts};
   
   my $r = eval {$c->model->сохранить_заявку($data
     #~ %$data,
@@ -270,7 +271,7 @@ sub черновик_заявки {
 sub ask_docx {
   my $c = shift;
   my $id = $c->vars('id');
-   $c->render(text=>$c->model->ask_docx($id));
+   $c->render(text=>$c->model->ask_docx($id)->{python});
 }
 
 
