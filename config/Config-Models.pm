f9b9ae28c0e69f9bbeb971ae6c2a8c4653418427
Mikhail Che <=>
2018-10-18 17:15:20 +0500

    дальше

--- config/Config-Models.pm
+++ config/Config-Models.pm
@@ -1,2 +1,18 @@
 use utf8;
-qw( Main Category  Object Project Nomen/Номенклатура Contragent Wallet Access Profile TimeWork Money Waltex::Report    TMC Transport MedCol Staff);#
+# !порядок
+qw( Main
+  Category
+  Object
+  Project
+  Nomen/Номенклатура
+  Contragent
+  Wallet
+  Access
+  Profile
+  TimeWork
+  Money
+  Waltex::Report
+  TMC Transport
+  Staff
+  MedCol
+  );#
