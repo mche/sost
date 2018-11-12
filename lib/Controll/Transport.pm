e8c5a581dca03a751c957af44e748f2dd892da10
Mikhail Che <=>
2018-11-12 09:48:56 +0500

    дальше

--- lib/Controll/Transport.pm
+++ lib/Controll/Transport.pm
@@ -338,7 +338,7 @@ sub ask_docx {
   my $data = $c->model->ask_docx($id);
   my $err_file = "$data->{docx_out_file}.error";
   
-  open(PYTHON, "| python3.5 2>'$err_file' ")
+  open(PYTHON, "| python 2>'$err_file' ")
     || die "can't fork: $!";
   #~ ##local $SIG{PIPE} = sub { die "spooler pipe broke" };
   say PYTHON $data->{python};
