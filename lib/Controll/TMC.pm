6e115c2dfee8fadbe1afcd4a3b299842c05fcc0b
Mikhail Che <=>
2019-01-25 18:40:09 +0500

    дальше

--- lib/Controll/TMC.pm
+++ lib/Controll/TMC.pm
@@ -22,6 +22,7 @@ sub index {
 
 sub index_snab {
   my $c = shift;
+  eval { $c->minion->enqueue(slow_log => ['test 123']) };
   return $c->render('tmc/snab',
     handler=>'ep',
     'header-title' => 'Учет ТМЦ',
--- script/app.pl
+++ script/app.pl
@@ -16,4 +16,21 @@ my $app = __PACKAGE__->new(config =>'config/Config.pm',);
 #~ $app->types(MojoX::MIME::Types->new);
 # инициация моделей - Это обязательно для hypnotoad, который запускает параллельные процессы и возникают конфликты postgresql tuple concurrently updated
 $app->init_models();
+
+
+  Mojo::IOLoop::Subprocess->new->run(
+    sub {
+      my $subprocess = shift;
+      #~ sleep 5;
+      $app->log->error("Minion has been stated pid $$");
+      $app->start('minion', 'worker');
+      return $$;
+    },
+    sub {
+      my ($subprocess, $err, @results) = @_;
+      $app->log->error('Minion has done ....', @results);
+      #~ say "Subprocess error: $err" and return if $err;
+    }
+  ) if $app->renderer->get_helper('minion');
+
 $app->start();# did not return an application object.
