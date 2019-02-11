7337037002a3700652991d416117884bdc3e1197
Mikhail Che <mche@cpan.org>
2019-02-11 16:38:13 +0500

    дальше

--- lib/Controll/Tg.pm
+++ lib/Controll/Tg.pm
@@ -6,15 +6,16 @@ has model => sub {shift->app->models->{'Tg'}};
 sub webhook {
   my $c = shift;
   my $token = $c->stash('token');
+  
   $c->app->log->error("Bad Tg token=[$token]")
-    and $c->reply->not_found()
+    and $c->reply->not_found
     unless $token eq $c->app->config->{'tg UniOST bot token'};
   
   my $data = $c->req->json;
   
   $c->app->log->info($c->dumper($data));
   
-  $c->render(json=>{"ok":1});
+  $c->render(json=>{"ok"=>1});
 }
 
 1;
