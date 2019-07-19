a5444ded817c9037994a31ed1203010010d809cf
Mikhail Che <mche@cpan.org>
2019-07-19 09:31:50 +0500

    дальше

---
+++ static/js/components/тмц/сертификаты/index.js
@@ -0,0 +1,56 @@
+(function () {'use strict';
+/*
+  Модуль ТМЦ сертификатов
+*/
+
+var moduleName = "Серификаты ТМЦ";
+try {angular.module(moduleName); return;} catch(e) { } 
+var module = angular.module(moduleName, ['TemplateCache', /*'Util', 'appRoutes',*/ /*'ТМЦ::Сертификаты::Объекты',*/ 'ТМЦ::Сертификаты::Закупки', 'ТМЦ::Сертификаты::Папки']);//'ngSanitize',, 'dndLists'
+
+var Controll = function  ($scope, $timeout, $element, TemplateCache, appRoutes, /*$КомпонентТМЦСертификатыОбъекты,*/ $КомпонентТМЦСертификатыЗакупки, $КомпонентТМЦСертификатыПапки) {
+  var ctrl = this;
+  var meth = {/*методы Vue*/};
+  
+  ctrl.$onInit = function(){
+    ctrl.param = {};
+    TemplateCache.split(appRoutes.url_for('assets', 'тмц/сертификаты.html'), 1)
+      .then(function(proms){
+        ctrl.ready= true;
+        ctrl.Vue();
+      });
+    
+  };
+  
+  ctrl.Vue = function(){$timeout(function(){
+    ctrl.vue = new Vue({
+      "el":  $element[0], //.childNodes[0],
+      //~ "delimiters": ['{%', '%}'],
+      "data"() {
+          return {
+            "ready": true,
+          };
+        },
+        "methods": meth,
+        "mounted"(){
+          var vm = this;
+        },
+        "components": {
+          //~ 'v-left-objects': new $КомпонентТМЦСертификатыОбъекты(),/// {/*"param": $c.param*/}, $c
+          'v-center-zakup': new $КомпонентТМЦСертификатыЗакупки(),
+          'v-right-folders': new $КомпонентТМЦСертификатыПапки(),
+        },
+      });
+    
+  });};
+  
+};
+
+/*=============================================================*/
+
+module
+
+.controller('Controll', Controll)
+
+;
+
+}());
