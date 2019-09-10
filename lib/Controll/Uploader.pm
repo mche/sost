504ce8c706a735becdf8432ef2f47d8ec05a9657
Mikhail Che <mche@cpan.org>
2019-09-10 18:11:01 +0500

    дальше

--- lib/Controll/Uploader.pm
+++ lib/Controll/Uploader.pm
@@ -8,7 +8,7 @@ sub выгрузить_файл {# выгрузить файл
   my $upload = $c->req->upload('file')
     or return $c->render(json=>{error=>'Нет файла'});
   $c->app->log->error($c->param('lastModified'));
-  $c->render(json=>{success=>{"filename"=> $upload->filename, "content_type"=>$upload->headers->content_type, "size"=>$upload->size, "last_modified"=>$c->param('lastModified') || $c->param('last_modified')}});#  currentChunkSize
+  $c->render(json=>{success=>{"filename"=> $upload->filename, "content_type"=>$upload->headers->content_type, "size"=>$upload->size, "last_modified"=>$c->param('lastModified') || $c->param('last_modified')}});#   currentChunkSize
 }
 
 1;
