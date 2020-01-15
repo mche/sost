0e3b9ba87f526f9a65bca4e71ca802ecefe860d6
Mikhail Che <mche@cpan.org>
2020-01-15 12:03:40 +0500

    дальше

--- sql/табель закрыть.sql
+++ sql/табель закрыть.sql
@@ -1,6 +1,6 @@
 update "разное"
   set 
-    val='{"117873": " 50 days ", "132387": " 50 days ", "132389": " 50 days ", "211310": " 50 days ", "249240": " 50 days ", "455343": " 50 days ", "675138": " 50 days ", "827181": " 50 days ", "872426": " 50 days "}'::jsonb,
+    val='{"117873": " 100 days ", "132387": " 100 days ", "132389": " 100 days ", "211310": " 100 days ", "249240": " 100 days ", "455343": " 100 days ", "675138": " 100 days ", "827181": " 100 days ", "872426": " 100 days "}'::jsonb,
     uid=0,
     ts=default 
 where key='месяц табеля закрыт/interval' and (ts+'12 hour'::interval) < now()---12 hour
