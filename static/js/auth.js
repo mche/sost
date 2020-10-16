61077ad0331ba2a77bb7d4a3c6f45f563b35806a
Mikhail Che <mche@cpan.org>
2020-10-16 13:27:30 +0500

    дальше

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,29 +1,30 @@
 import {AuthForm, ModalAuth} from './svelte-auth/dist/index.mjs';
 /** static/js/svelte-auth $ npm run build
 ** parcel build --no-source-maps --log-level 3 -d ../dist/ ../auth.js
+** в ассетпак js/dist/auth.js
 **/
 
 // нет так больше в два раза, чем выше
 //~ import AuthForm from './svelte-auth/src/AuthForm.svelte';
 //~ import ModalAuth from './svelte-auth/src/ModalAuth.svelte';
 
-// в ассетпак js/dist/auth.js
 
-//~ import md5 from 'nano-md5';
-
-const DOMVersion = ()=>{
-  return document.querySelector('head meta[name="app:version"]').getAttribute('content');
+const DOMVersion = (version)=>{
+  let dom = document.querySelector('head meta[name="app:version"]');
+  if (version) dom.setAttribute('content', version);
+  else return dom.getAttribute('content');
 };
 
-const StorageVersion = (test, set)=>{
+const CheckVersion = (test, set)=>{
   let ver = window.localStorage.getItem('app:version '+window.location.pathname);
-  if (test) {
-    if (!ver ||  test != ver) {
+  let dom = DOMVersion();
+  //~ if (test) {
+    if (!ver ||  test != ver || test != dom) {
       window.localStorage.setItem('app:version '+window.location.pathname, set || test);
       return false;
     } else return true;
-  }
-  return ver;
+  //~ }
+  //~ return ver;
 };
 
 const ShowUpdate = (msg)=>{
@@ -31,14 +32,14 @@ const ShowUpdate = (msg)=>{
   return new Promise((resolve, reject) => {
     Materialize.Toast($('<a href="javascript:" class="hover-shadow3d red-text text-darken-4">')
       .click(function(){ document.getElementById('toast-container').remove(); resolve();  })
-      .html('Обновите [F5] страницу <i class="material-icons" style="">refresh</i> '+msg), 30000, 'red lighten-4 red-text text-darken-4 border fw500 animated zoomInUp');
+      .html('Обновите [F5] страницу <i class="material-icons" style="">refresh</i> '+msg), 30*1000, 'red lighten-4 red-text text-darken-4 border fw500 animated zoomInUp');
     
   });
   
 };
 
 const ReloadCache = (version)=>{
-  StorageVersion(version, version);
+  CheckVersion(version, version);
   window.location.reload(true);
 };
 
@@ -50,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   if (app) auth = new AuthForm({"target": app, "props":{"Success":(data)=>{
     setTimeout(()=>{
       auth.$destroy();
-      if (data.version && !StorageVersion(data.version, data.version) ) window.location.reload(true);
+      if (data.version && !CheckVersion(data.version, data.version) ) window.location.reload(true);
       else window.location.reload();
     }, 500);///анимацию
   }}});
@@ -64,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       if (modalAuth) return;
       modalAuth = new ModalAuth({"target": document.body, "props":{"Success":(data)=>{
         setTimeout(()=>{ modalAuth.$destroy(); modalAuth = undefined; }, 1000);///анимацию
-        if (data.version && !StorageVersion(data.version, 1)) ShowUpdate(data.version).then(()=>{ ReloadCache(data.version); });
+        if (data.version && !CheckVersion(data.version, 1)) ShowUpdate(data.version).then(()=>{ ReloadCache(data.version); });
       }}});
     };
     
@@ -76,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       //~ 
       fetch('/keepalive').then((resp)=>{
         if (resp.ok) return resp.json().then((data)=>{
-          if (data.version && !StorageVersion(data.version,1)) ShowUpdate(data.version).then(()=>{ ReloadCache(data.version); });
+          if (data.version && !CheckVersion(data.version,1)) ShowUpdate(data.version).then(()=>{ ReloadCache(data.version); });
         }); /// строка версии
         else Materialize.Toast($('<a  href="javascript:" class="hover-shadow3d white-text bold">').click(function(){ ShowModalAuth(); document.getElementById('toast-container').remove(); }).html('Завершилась авторизация, войти заново <i class="icon-login"></i>'), 30*1000, 'red darken-2 animated zoomInUp');
         
@@ -93,7 +94,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
   if ( document.querySelector('head meta[name="app:uid"]').getAttribute('content') && !document.querySelector('.status404') ) {
     var curr = DOMVersion();
-    if (!StorageVersion(curr, curr)) {
+    if (!CheckVersion(curr, curr)) {
       //~ console.log("Перезапуск страницы с новой версией: ", ver);
       //~ Materialize.Toast($('<a href="javascript:" class="hover-shadow3d green-text text-darken-4">').click(function(){ window.location.reload(true); }).html('Обновление <i class="material-icons" style="">refresh</i> '+curr), 5000, 'green lighten-4 green-text text-darken-4 border fw500 animated zoomInUp');
       //~ ReloadCache(curr);
