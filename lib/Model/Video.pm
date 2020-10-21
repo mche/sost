2e854eae22153157d11e1937fe9ac98346f639ad
Mikhail Che <mche@cpan.org>
2020-10-21 10:57:22 +0500

    дальше

--- lib/Model/Video.pm
+++ lib/Model/Video.pm
@@ -5,22 +5,30 @@ use Mojo::Base 'Model::Base';
 
 has [qw(app)];
 
-has cam1 => qq(ffmpeg  -i 'rtsp://192.168.128.18:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0   - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
-has cam11 => qq(ffmpeg  -i 'rtsp://192.168.128.11:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0   - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
-has cam12 => qq(ffmpeg  -i 'rtsp://192.168.128.12:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0   - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
+has cam1 => qq(ffmpeg  -i 'rtsp://192.168.128.18:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0 -s 1280x720   - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
+has cam11 => qq(ffmpeg  -i 'rtsp://192.168.128.11:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0  -s 1280x720 - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
+has cam12 => qq(ffmpeg  -i 'rtsp://192.168.128.12:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0 -s 1280x720  - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
+has cam14 => qq(ffmpeg  -i 'rtsp://192.168.128.14:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0 -s 1280x720  - 2>/dev/null |);
+has cam15 => qq(ffmpeg  -i 'rtsp://192.168.128.15:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0 -s 1280x720  - 2>/dev/null |); #-s 1280x720 -crf  50 -an 
+has cam16 => qq(ffmpeg  -i 'rtsp://192.168.128.16:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k   -bf 0 -s 1280x720   - 2>/dev/null |); #-crf  50 -an 
+has cam17 => qq(ffmpeg  -i 'rtsp://192.168.128.17:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k -s 1280x720  -bf 0   - 2>/dev/null |); 
+#~ has cam18 => qq(ffmpeg  -i 'rtsp://192.168.128.18:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k -s 1280x720  -bf 0   - 2>/dev/null |); 
+has cam19 => qq(ffmpeg  -i 'rtsp://192.168.128.19:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k -s 1280x720  -bf 0   - 2>/dev/null |); 
+has cam20 => qq(ffmpeg  -i 'rtsp://192.168.128.20:554/user=admin&password=&channel=1&stream=0.sdp?' -f mpegts -codec:v mpeg1video  -b:v 1300k -s 1280x720  -bf 0   - 2>/dev/null |); 
+
+# https://ffmpeg.org/ffmpeg-filters.html#xstack
+#~ has cams => qq(ffmpeg -i 'rtsp://192.168.128.16:554/user=admin&password=&channel=1&stream=0.sdp?'  -i 'rtsp://192.168.128.19:554/user=admin&password=&channel=1&stream=0.sdp?' -i 'rtsp://192.168.128.20:554/user=admin&password=&channel=1&stream=0.sdp?' -i 'rtsp://192.168.128.17:554/user=admin&password=&channel=1&stream=0.sdp?'   -filter_complex "xstack=inputs=4:layout=0_0|0_h0|0_h0+h1|0_h0+h1+h2" -f mpegts -codec:v mpeg1video  -s 1280x2160 -bf 0 - 2>/dev/null |); #[0:v]scale=1920:-1 [s0]; [3:v]scale=1920:-1 [s3] ;   [s0][1:v][2:v][s3]xstack=inputs=4:layout=0_0|0_h0|0_h0+h1|0_h0+h1+h2 тут масштабируеются две (0 и 3 инпуты) из 4х камер до ширины 1920 () и все в один столбик РАЗМЕР! не более 4095!!! для этого кодека
 
 has feeds => sub {
   my $self = shift;
   my $feeds = {};
   
-  my $cam1 = $self->stream($self->cam1);
-  $self->start_feed('cam1', $cam1, $feeds);
-  
-  my $cam11 = $self->stream($self->cam11);
-  $self->start_feed('cam11', $cam11, $feeds);
-  
-  my $cam12 = $self->stream($self->cam12);
-  $self->start_feed('cam12', $cam12, $feeds);
+  map {
+    my $cam = $self->stream($self->$_);
+    $self->start_feed($_, $cam, $feeds);
+    
+  } qw(cam1 cam11 cam12 cam14 cam15 cam16 cam17 cam19 cam20);
+
   
   return $feeds;
 };
