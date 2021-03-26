2f685fd5090ebb3e5d8fea46a0ae1cfcb50abea9
Mikhail Che <mche@cpan.org>
2021-03-26 15:14:43 +0500

    дальше

--- lib/Model/Waltex/Report/Rent.pm
+++ lib/Model/Waltex/Report/Rent.pm
@@ -20,7 +20,7 @@ sub движение_арендатора {
   $self->dbh->selectall_arrayref($self->sth('движение арендатора', select=>' *, "приход"::numeric as "приход/num", "расход"::numeric as "расход/num" ', where=>$where, order_by=>' order by "дата", "sign" '), {Slice=>{}}, @bind);
 }
 
-sub долги {# пока по всему контрагенту
+sub долги {# пока по всему контрагенту общий долг
   my ($self, $param) = (shift, shift);
   my ($where, @bind) = $self->SqlAb->where({
     ' "кошельки/id"[1][1] '=>$param->{'арендодатель'},
