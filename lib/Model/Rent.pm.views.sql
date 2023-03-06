afdb0e34c643f70ff540bde3dfcf2eeecd8d4fee
Михаил Че (Mikhail Che) <mche@users.noreply.github.com>
2023-03-06 14:16:56 +0500

    Update Rent.pm.views.sql

--- lib/Model/Rent.pm.views.sql
+++ lib/Model/Rent.pm.views.sql
@@ -162,7 +162,7 @@ with agg as (
     || dop."@доп.согл./дата1"
     || case
           when d."дата расторжения" is null and not coalesce(d."продление срока", false) then d."дата2"
-          else d."дата расторжения"+interval '1 day'
+          else d."дата расторжения"
           end as "@даты",--- с продлением тут нулл
     dop."@доп.согл./id"
   from "аренда/договоры" d
